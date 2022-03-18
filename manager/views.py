2f6cfb0591ea975ea83173220791d3e64e099947
AbdelrahmanNabill <jrnabil95@gmail.com>
2022-03-18 20:45:22 +0200

    lock and unlock admin functions

--- manager/views.py
+++ manager/views.py
@@ -1,1 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from users.models import Profile
+from django.contrib.auth.models import User
+from users.logger import log
+from users.util_funcs import *
+
+def Admin_lock_user(request, id):
+
+  ### lock a specific user not to be able to login again but his account still exist
+   
+
+    if(is_authorized_admin(request)):
+        user = User.objects.get(pk=id)
+        lock_user(user)
+        log(request.user.username+" locked " + user.username+".")
+        return HttpResponseRedirect("redirect path")
+    else:
+        return HttpResponseRedirect("redirect path")
+
+
+def Admin_unlock_user(request, id):
+
+    ### unlock a specific user so he can login again
+    
+
+    if(is_authorized_admin(request)):
+        user = User.objects.get(pk=id)
+        unlock_user(user)
+        log(request.user.username+" unlocked " + user.username+".")
+        return HttpResponseRedirect("redirect path")
+    else:
+        return HttpResponseRedirect("redirect path")
+        
+        
+        
+
+
