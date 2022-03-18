4292f3dd2717afa4ec6c7c084f24da2e090d73e0
mahmoud <mahmoudkamal448>
2022-03-18 21:48:14 +0200

    Customize the Django Admin Version 1.1

--- category/admin.py
+++ category/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Category, Post
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
+
+class InlinePost(admin.StackedInline):
+    model = Post
+    extra = 1
+
+
+class CustomCategory(admin.ModelAdmin):
+    inlines = [InlinePost]
+
+
+class CustomPost(admin.ModelAdmin):
+    fieldsets = (
+        ['write a new post', {'fields': ['title', 'slug', 'author']}],
+        ['post content', {'fields': ['content']}],
+        ['Publish date', {'fields': ['publish_date']}]
+    )
+    list_display = ('title', 'slug', 'author', 'content', 'publish_date')
+    search_fields = ('title', 'slug', 'author')
+    list_filter = ('title', )
+
+
+admin.site.register(Category, CustomCategory)
+admin.site.register(Post, CustomPost)
