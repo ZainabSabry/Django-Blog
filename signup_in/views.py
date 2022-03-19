7cb61b4f0ef53b9b75b233589961360181caa95e
saragalal <galalsara0@gmail.com>
2022-03-19 18:44:42 +0200

    signin&sign up views and template functional

--- signup_in/views.py
+++ signup_in/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
+from .forms import CreateUserForm
+
+
+def registerPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'signup_in/register.html', context)
+
+
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/admin')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+    context = {}
+    return render(request, 'signup_in/login.html', context)
