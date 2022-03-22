2a8947a5ab79100947144185ff1fd7535255423d
jimmy <muhammedgamal760@gmail.com>
2022-03-22 23:43:56 +0200

    subscription functionality

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from category.models import Category, Post
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -18,5 +18,14 @@ def post(request):
     context = {'posts': posts}
     return render(request, 'landing_page/post.html', context)
 
-
+def sub(request,cat):
+    category = Category.objects.get(id=cat)
+    if request.method == 'POST':
+        if category.is_subscribed == False:
+            category.is_subscribed = True
+            return redirect('../../landing_page/home/')
+        else:
+            category.is_subscribed = False
+            return redirect('../../landing_page/home/')
+    # return redirect('home')
 
